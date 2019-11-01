c98ebbf30c09f5c0495d5b1a7378bfe78047ead8
Valentine <valentine@Valentine-lp>
2019-11-01 15:51:31 +0300

    hw4

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -5,15 +5,23 @@
 
 #include <sys/epoll.h>
 
+#include <spdlog/logger.h>
+
+#include <afina/Storage.h>
+
+#include <deque>
 namespace Afina {
 namespace Network {
 namespace STnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps) : _socket(s), pStorage(ps)
+    {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
+        _first_offset=0;
+        
     }
 
     inline bool isAlive() const { return true; }
@@ -32,6 +40,14 @@ private:
 
     int _socket;
     struct epoll_event _event;
+    bool _isAlive;
+    std::shared_ptr<spdlog::logger> _logger;
+    static const int r_mask = EPOLLIN | EPOLLRDHUP | EPOLLERR;
+    static const int rw_mask = EPOLLIN | EPOLLRDHUP | EPOLLERR | EPOLLOUT;
+    std::shared_ptr<Afina::Storage> pStorage;
+
+    std::deque<std::string> _results;
+    int _first_offset;
 };
 
 } // namespace STnonblock
