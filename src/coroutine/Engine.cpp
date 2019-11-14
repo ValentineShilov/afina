fd4ecefc4429b9acecdf9ec73c2e0f3f0855b8e5
Valentine <valentine@Valentine-lp>
2019-11-14 22:15:03 +0300

    coroutines

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -3,25 +3,25 @@
 #include <setjmp.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <iostream>
 namespace Afina {
 namespace Coroutine {
 
 
-
+ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Engine::Store(context &ctx)
 {
-  char v(0);
-//  {
+  char s_end[1];
+  {
     //high addr StackBottom
     //
     //
     //low addr  v
 
     ctx.Hight = StackBottom;
-    ctx.Low = &v;
+    ctx.Low = s_end;
 
-
+    std::cout <<"store:" <<  (int64_t)ctx.Hight <<" " <<(int64_t)ctx.Low   <<std::endl;
     uint32_t &old_size = std::get<1>(ctx.Stack);
     char * &buffer = std::get<0>(ctx.Stack);
     auto new_size = ctx.Hight - ctx.Low;
@@ -29,21 +29,25 @@ void Engine::Store(context &ctx)
     if (old_size < new_size || buffer==nullptr)
     {
         //not enough space for stack -> increase size
-        delete[] buffer;
+        if(buffer!=nullptr)
+            delete[] buffer;
         buffer = new char[new_size];
         old_size = new_size;
     }
 
     memcpy(buffer, ctx.Low, new_size);
-//  }
+  }
 }
+ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Engine::Restore(context &ctx)
 {
-    char s_end[2];
-    {
 
-      if (s_end >= ctx.Low && s_end <= ctx.Hight)
+    char s_end[1];
+    {
+      std::cout <<"REstore:" << (uint64_t)s_end <<std::endl;
+      if (s_end >= ctx.Low)
       {
+          std::cout <<"Restore recursive call:" << (uint64_t)ctx.Hight <<std::endl;
           Restore(ctx);
       }
       size_t size = ctx.Hight - ctx.Low;
@@ -54,13 +58,19 @@ void Engine::Restore(context &ctx)
       longjmp(ctx.Environment, 1);
     }
 }
+ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Engine::yield()
 {
-
+  std::cout <<"Yield:" <<(uint64_t)cur_routine <<std::endl;
   auto c = alive;
-  if(alive==cur_routine)
+  if(cur_routine!=nullptr)
   {
-     c = alive->next;
+
+    if(alive==cur_routine)
+    {
+       c = alive->next;
+    }
+
   }
   if(c!=nullptr)
   {
@@ -68,8 +78,11 @@ void Engine::yield()
   }
 
 }
+
+ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Engine::sched(void *routine)
 {
+  std::cout <<"Sched:" <<(uint64_t)cur_routine <<" "  <<(uint64_t)routine <<std::endl;
   if (cur_routine == routine)
   {
     return;
@@ -86,15 +99,17 @@ void Engine::sched(void *routine)
 
 }
 
-
+ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Engine::Switch(context& ctx)
 {
+    std::cout <<"Sched:" <<(uint64_t)cur_routine <<" "  <<(uint64_t)&ctx <<std::endl;
     if(cur_routine && cur_routine != idle_ctx)
     {
         if (setjmp(cur_routine->Environment) > 0)
         {
             //we successfully switched to another coroutine
             //and then returned to this coroutine
+            return;
         }
         else
         {
@@ -105,6 +120,11 @@ void Engine::Switch(context& ctx)
           Restore(ctx);
         }
 
+    }
+    else
+    {
+      cur_routine = &ctx;
+      Restore(ctx);
     }
 
 }
